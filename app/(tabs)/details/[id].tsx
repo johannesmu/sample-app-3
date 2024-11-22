a0c3c2a1038f8c706e4dafd3ce3d7b1675ac4e2b
johannes <johannes@bx.o.a>
2024-11-22 11:01:45 +1100

    update to detail page issues

---
+++ app/(tabs)/details/[id].tsx
@@ -0,0 +1,135 @@
+import { Text, View, StyleSheet,  Pressable, TextInput, Switch } from 'react-native'
+import { useLocalSearchParams, Link, useNavigation } from 'expo-router'
+import { useEffect, useContext, useState } from 'react'
+import { FirestoreContext } from '@/contexts/FirestoreContext'
+import { AuthenticationContext } from '@/contexts/AuthenticationContext'
+import { doc, getDoc } from '@firebase/firestore'
+import { ItemPrototype } from '@/interfaces/ItemInterface'
+
+export default function DetailScreen(props: any) {
+    const [documentData, setDocumentData] = useState<ItemPrototype | any>()
+    const [ docName, setDocName ] = useState<string>('')
+    const [ docStatus, setDocStatus ] = useState<boolean>( false )
+    const [ edited, setEdited ] = useState<boolean>( false )
+    const [ dataLoaded, setDataLoaded ] = useState<boolean>( false )
+   
+
+    // access navigation object via hook
+    const navigation = useNavigation()
+    // set screen options
+    useEffect( () => {
+        navigation.setOptions({ headerShown: true })
+        console.log( navigation.isFocused() )
+    }, [navigation])
+
+    useEffect( () => {
+        if( navigation.isFocused() ) {
+            getDocument()
+        }
+    }, [ navigation ])
+   
+
+    const { id }: any = useLocalSearchParams()
+    const { name }:any = useLocalSearchParams()
+    const { status }:boolean = useLocalSearchParams()
+
+
+    const db = useContext(FirestoreContext)
+    const auth = useContext(AuthenticationContext)
+
+    const getDocument = async () => {
+        console.log( auth )
+        const ref = doc(db, `users/${auth.currentUser.uid}/documents`, id)
+        const document = await getDoc(ref)
+        let data: ItemPrototype | any = document.data()
+        data.id = id
+        setDocumentData(data)
+        setDocName( data.name )
+        setDocStatus( data.status )
+        setDataLoaded( true )
+        console.log("dataLoaded")
+    }
+
+    useEffect( () => { 
+        if( dataLoaded == false && auth ) {
+            getDocument()
+        }
+    },[dataLoaded])
+
+
+    if (!documentData) {
+        return null
+    }
+    else {
+        return (
+            <View style={styles.page}>
+                <View style={styles.itemHeader}>
+                    <Text style={styles.itemHeaderText}>Detail for  {name}</Text>
+                </View>
+                <View style={styles.container}>
+                    <Text>Document name</Text>
+                    <TextInput 
+                        value={ docName} 
+                        onChangeText={
+                            (val) => {
+                                setDocName(val)
+                                setEdited( true )
+                            }
+                        }
+                    />
+                    <Text>Document status</Text>
+                    <Switch 
+                        value={docStatus} 
+                        onValueChange={
+                            () =>{ 
+                                (docStatus) ? setDocStatus(false) : setDocStatus(true) 
+                                setEdited( true )
+                            }
+                        }
+                    />
+                    <Pressable style={ (edited) ? styles.editButton : styles.editButtonDisabled }>
+                        <Text style={ styles.editButtonText}>Save Changes?</Text>
+                    </Pressable>
+                </View>
+
+                <Link href="/(tabs)/list" style={ styles.backButton }>
+                    <Text>Go back</Text>
+                </Link>
+            </View>
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        padding: 10,
+    },
+    itemHeader: {
+        backgroundColor: "#f1f7b7",
+        padding: 10,
+    },
+    itemHeaderText: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    backButton: {
+        padding: 12,
+    },
+    editButton: {
+        padding: 10,
+        backgroundColor: "darkblue",
+        marginVertical: 20,
+    },
+    editButtonText: {
+        color: "white",
+        textAlign: "center"
+    },
+    editButtonDisabled: {
+        display: "none"
+    }
+})
