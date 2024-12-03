a155687d53520959865b413d12b0a50cb37c46e5
johannes <jmuljana@gmail.com>
2024-12-03 17:35:12 +1100

    styling modal

--- app/(details)/[id].tsx
+++ app/(details)/[id].tsx
@@ -122,7 +122,7 @@ const styles = StyleSheet.create({
     },
     modalContainer: {
         flex: 1,
-        justifyContent: "center",
+        padding: 20,
     },
     page: {
         flex: 1,
@@ -130,10 +130,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 10,
+        marginTop: 100,
     },
     modalButton: {
         position: "absolute",
         right: 10,
         top: 10,
+        zIndex: 100,
     }
 })
