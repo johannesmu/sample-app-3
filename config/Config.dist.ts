25bb9769f7a243e79fe43381c371e13d86b6892b
johannes <johannes@ait.nsw.edu.au>
2025-04-04 10:45:27 +1100

    added config to gitignore and added dist version

---
+++ config/Config.dist.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "Your-api-key",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+}
+// replace with your firebase config from your project
--- config/Config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCm0r9afJGUoqMTJKqamOkv64bkWs7Hbuk",
-  authDomain: "expo24-cf78d.firebaseapp.com",
-  projectId: "expo24-cf78d",
-  storageBucket: "expo24-cf78d.appspot.com",
-  messagingSenderId: "152789493379",
-  appId: "1:152789493379:web:7dcd534f89b08efeda5db6"
-}
